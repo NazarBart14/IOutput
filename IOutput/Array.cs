de94d997a98592bc1fad38dddabeaf2ea44afe13
Lenovo <Lenovo@DESKTOP-AJ5INH1>
2022-09-11 11:48:30 +0300

    Добавьте файлы проекта.

---
+++ IOutput/Array.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOutput
+{
+    internal class Array : IAOutput
+    {
+    
+        public void Show()
+        {
+            Console.WriteLine($"Dima");
+        }
+        public void Shows(string info)
+        {
+            Console.WriteLine($"Kolja");
+        }
+        public void Info()
+        {
+            Console.WriteLine($"Nazar");
+        }
+    }
+}
---
+++ IOutput/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOutput
+{
+    public interface IAOutput
+        {
+             void Show();
+             void Shows(string info);
+        }
+    internal class Program : Array
+    {
+        
+        
+        static void Main(string[] args)
+        {
+           
+            Array array = new Array();
+            string info = null;
+            array.Show();
+            array.Shows(info);
+            array.Info();
+        }
+    }
+}
